de7a7d0102490be01ce1f74938e85bb6236d2d81
kennojc <kennojc@gmail.com>
2020-10-19 23:54:24 -0300

    Casi listo, faltan los iguales

--- mayor_de_4.rb
+++ mayor_de_4.rb
@@ -3,8 +3,11 @@ number_2 = ARGV[1].to_i
 number_3 = ARGV[2].to_i
 number_4 = ARGV[3].to_i
 
+def threenum()
+  number_1 = ARGV[0].to_i
+  number_2 = ARGV[1].to_i
+  number_3 = ARGV[2].to_i
 
-if ARGV[3].nil?
   if (number_1 > number_2) && (number_1 > number_3)
     puts "#{number_1}"
   elsif (number_2 > number_1) && (number_2 > number_3)
@@ -12,4 +15,27 @@ if ARGV[3].nil?
   else
     puts "#{number_3}"
   end
+end
+
+def fournum()
+  number_1 = ARGV[0].to_i
+  number_2 = ARGV[1].to_i
+  number_3 = ARGV[2].to_i
+  number_4 = ARGV[3].to_i
+  
+  if (number_1 > number_2) && (number_1 > number_3) && (number_1 > number_4)
+    puts "#{number_1}"
+  elsif (number_2 > number_1) && (number_2 > number_3) && (number_2 > number_4)
+    puts "#{number_2}"
+  elsif (number_3 > number_1) && (number_3 > number_2) && (number_3 > number_4)
+    puts "#{number_3}"
+  else
+    puts "#{number_4}"
+  end
+end
+
+if ARGV[3].nil?
+  threenum
+else
+  fournum
 end
