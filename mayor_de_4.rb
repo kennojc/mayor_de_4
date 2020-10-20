10053e002e727d32a4b27b503ca57c756646ee14
kennojc <kennojc@gmail.com>
2020-10-19 23:56:32 -0300

    Desafío listo

--- mayor_de_4.rb
+++ mayor_de_4.rb
@@ -8,9 +8,9 @@ def threenum()
   number_2 = ARGV[1].to_i
   number_3 = ARGV[2].to_i
 
-  if (number_1 > number_2) && (number_1 > number_3)
+  if (number_1 >= number_2) && (number_1 >= number_3)
     puts "#{number_1}"
-  elsif (number_2 > number_1) && (number_2 > number_3)
+  elsif (number_2 >= number_1) && (number_2 >= number_3)
     puts "#{number_2}"
   else
     puts "#{number_3}"
@@ -22,12 +22,12 @@ def fournum()
   number_2 = ARGV[1].to_i
   number_3 = ARGV[2].to_i
   number_4 = ARGV[3].to_i
-  
-  if (number_1 > number_2) && (number_1 > number_3) && (number_1 > number_4)
+
+  if (number_1 >= number_2) && (number_1 >= number_3) && (number_1 >= number_4)
     puts "#{number_1}"
-  elsif (number_2 > number_1) && (number_2 > number_3) && (number_2 > number_4)
+  elsif (number_2 >= number_1) && (number_2 >= number_3) && (number_2 >= number_4)
     puts "#{number_2}"
-  elsif (number_3 > number_1) && (number_3 > number_2) && (number_3 > number_4)
+  elsif (number_3 >= number_1) && (number_3 >= number_2) && (number_3 >= number_4)
     puts "#{number_3}"
   else
     puts "#{number_4}"
